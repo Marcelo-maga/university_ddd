8a35db5a8cfa8b0e8a8b6bc7fdad13a50b161200
Joaby <joabyoliveira2004@gmail.com>
2023-08-31 00:56:18 -0300

    docs: document alunos routes

--- src/server/routes/alunos.routes.ts
+++ src/server/routes/alunos.routes.ts
@@ -1,14 +1,26 @@
-import { Router as ExpressRouter } from 'express'
-import { AlunoContoller } from '../controllers/AlunoController'
+import { Router as ExpressRouter } from "express";
+import { AlunoContoller } from "../controllers/AlunoController";
 
 export const alunosRoutes = (router: ExpressRouter) => {
-    const alunoController: AlunoContoller = new AlunoContoller()
+  const alunoController: AlunoContoller = new AlunoContoller();
 
-    // TODO: alterar nome da rota
-    router.get('/getAllAlunos', async (request, response) => {
-        const result = await alunoController.getAllAlunos(request, response)
-        response.json({ teste: result })
-    })
+  // TODO: alterar nome da rota
+  /**
+   * @openapi
+   *  /getAllAlunos:
+   *   get:
+   *    description: Rota de get aluno
+   *    responses:
+   *    '200':
+   *      description: Sucesso
+   *    '500':
+   *      description: Error
+   *
+   */
+  router.get("/getAllAlunos", async (request, response) => {
+    const result = await alunoController.getAllAlunos(request, response);
+    response.json({ teste: result });
+  });
 
-    return router
-}
+  return router;
+};
