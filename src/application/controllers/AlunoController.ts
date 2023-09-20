b48c189bd26742403727640ac2987e442f2133e6
VictorNAGomes <dev.victorgomes@gmail.com>
2023-09-20 17:18:13 -0300

    feat: add new discipline to studant use case

--- src/application/controllers/AlunoController.ts
+++ src/application/controllers/AlunoController.ts
@@ -1,20 +1,26 @@
 import { Request, Response } from "express"
 import { AlunoRepository } from "../../infrastructure/repositories/AlunoRepository"
+import { DisciplinaRepository } from "../../infrastructure/repositories/DisciplinaRepository";
+import { addNewDiscipline } from "../../use-cases/addNewDiscipline";
 import Aluno from "../../domain/Aluno";
 
 export class AlunoController {
     private alunoRepository: AlunoRepository;
+    private disciplinaRepository: DisciplinaRepository;
+    private addNewDisc: addNewDiscipline;
 
     constructor() { 
         this.alunoRepository = new AlunoRepository()
+        this.disciplinaRepository = new DisciplinaRepository()
+        this.addNewDisc = new addNewDiscipline(this.alunoRepository, this.disciplinaRepository)
     }
 
     async getAllAlunos(request: Request, response: Response): Promise<Aluno[]> {
         return await this.alunoRepository.getAll()
     }
 
-    async addNewDiscipline(request: Request, response: Response): Promise<Aluno> {
+    async addNewDiscipline(request: Request, response: Response): Promise<Aluno | string> {
         const { alunoId, disciplinaId } = request.body
-        return await this.alunoRepository.addNewDiscipline(alunoId, disciplinaId)
+        return await this.addNewDisc.execute(alunoId, disciplinaId)
     }
 }
