14077e6de1d3d37215bb98b14091379cf01c7186
VictorNAGomes <dev.victorgomes@gmail.com>
2023-10-08 16:26:35 -0300

    fix: type error in alunoRepository

--- src/secretary/infrastructure/interfaces/IAlunoRepository.d.ts
+++ src/secretary/infrastructure/interfaces/IAlunoRepository.d.ts
@@ -1,11 +1,11 @@
 import { Aluno } from "../../../domains/secretary";
 
 export interface IAlunoRepository {
-    getAll: () => Promise<Aluno[]>;
-    get: (alunoId: number) => Promise<Aluno | null>;
-    create: (Aluno: Omit<Aluno, "AlunoId">) => Promise<Aluno>;
-    update: (alunoId: number, Aluno: Partial<Aluno>) => Promise<Aluno>;
-    delete: (alunoId: number) => Promise<Aluno>;
+  getAll: () => Promise<Aluno[]>;
+  get: (alunoId: number) => Promise<Aluno | null>;
+  create: (Aluno: Omit<Aluno, "alunoId">) => Promise<Aluno>;
+  update: (alunoId: number, Aluno: Partial<Aluno>) => Promise<Aluno>;
+  delete: (alunoId: number) => Promise<Aluno>;
 
-    addNewDiscipline: (alunoId: number, disciplinaId: number) => Promise<Aluno>;
+  addNewDiscipline: (alunoId: number, disciplinaId: number) => Promise<Aluno>;
 }
