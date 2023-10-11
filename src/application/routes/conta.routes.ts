e337da4216c3bf872d4fa018afc7b10d5718ad50
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2023-10-10 22:37:53 -0300

    feat: new routes and swagger

--- src/application/routes/conta.routes.ts
+++ src/application/routes/conta.routes.ts
@@ -1,8 +1,75 @@
 import { Router as ExpressRouter } from "express";
 import { ContaController } from "../controllers/ContaController";
+import { ControllerFactory } from "../../shared.kernel/factory";
 
 export const contaRoutes = (router: ExpressRouter) => {
-  const contaController: ContaController = new ContaController();
+  const contaController: ContaController = ControllerFactory.createContaController();
+
+  /**
+   * @swagger
+   * tags:
+   *   name: Conta
+   *   description: Endpoints relacionados a Conta
+   */
+
+  /**
+   * @swagger
+   * /conta:
+   *   post:
+   *     summary: Cria a conta
+   *     tags: [Conta]
+   *      requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/produto'
+   *     responses:
+   *       200:
+   *         description: Conta criada com sucesso
+   *       400:
+   *         description: Erro ao criar conta
+   *       500:
+   *         description: Erro interno
+   */
+  router.post("/conta", async (req, res) => {
+    try {
+      const conta = await contaController.createConta(req.body);
+      res.status(201).json(conta);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+  /**
+   * @swagger
+   * /conta/{id}:
+   *   delete:
+   *     summary: Deleta a conta
+   *     tags: [Conta]
+   *     parameters:
+   *       - in: path
+   *         name: id
+   *         required: true
+   *         description: ID da conta a ser apagada
+   *         schema:
+   *           type: string
+   *     responses:
+   *       200:
+   *         description: Conta deletada com sucesso
+   *       400:
+   *         description: Erro ao deletar conta
+   *       500:
+   *         description: Erro interno
+   */
+  router.delete("/conta/:id", async (req, res) => {
+    try {
+      const conta = await contaController.deleteConta(Number(req.params.id));
+      res.status(200).json(conta);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  });
 
   return router;
 };
--- src/application/routes/pagamento.routes.ts
+++ src/application/routes/pagamento.routes.ts
@@ -1,8 +1,47 @@
 import { Router as ExpressRouter } from "express";
 import { PagamentoController } from "../controllers/PagamentoController";
+import { ControllerFactory } from "../../shared.kernel/factory";
 
 export const pagamentoRoutes = (router: ExpressRouter) => {
-  const pagamentoController: PagamentoController = new PagamentoController();
+  const pagamentoController: PagamentoController =
+    ControllerFactory.createPagamentoController();
+
+  /**
+   * @swagger
+   * tags:
+   *   name: Pagamento
+   *   description: Endpoints relacionados a Pagamento
+   */
+
+  /**
+   * @swagger
+   * /pagamento/{id}:
+   *   delete:
+   *     summary: Fecha a conta
+   *     tags: [Pagamento]
+   *     parameters:
+   *       - in: path
+   *         name: id
+   *         required: true
+   *         description: ID da conta
+   *         schema:
+   *           type: string
+   *     responses:
+   *       200:
+   *         description: Pagamento feito com sucesso
+   *       400:
+   *         description: Erro ao efetuar o pagamento
+   *       500:
+   *         description: Erro interno
+   */
+  router.post("/pagamento/:id", async (req, res) => {
+    try {
+      const conta = await pagamentoController.fechaPagamento(req.body);
+      res.status(201).json(conta);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  });
 
   return router;
 };
